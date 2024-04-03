77d2944aa0fe01e3f3e0ebd854cdc2cc2fd69a52
BharathAnishetty <bharathanishetty@test.com>
2024-04-03 13:57:48 +0100

    Added CDN Tests

--- tests/verify-website.tests.ps1
+++ tests/verify-website.tests.ps1
@@ -13,6 +13,28 @@ Describe "Storage Account Validation" {
         $storageAccountExists | Should -Be $storageAccountName
     }
 }
+
+# Test to validate CDN Profile.
+Describe "CDN Profile Validation" {  
+
+    It "CDN Profile exists" {
+        Write-Host "cdnProfileName: $cdnProfileName"
+        $cdnProfileExists = az cdn profile show --name $cdnProfileName --resource-group $resourceGroupName --query "name" --output tsv
+        Write-Host "cdnProfileExists: $cdnProfileExists"
+        $cdnProfileExists | Should -Be $cdnProfileName
+    }
+}
+
+# Test to validate CDN Endpoint.
+Describe "CDN Endpoint Validation" {    
+    It "CDN Endpoint exists" {
+        Write-Host "cdnProfileName: $cdnProfileName"
+        Write-Host "cdnProfileEndPoint: $endpointName"
+        $endpointExists = az cdn endpoint list --profile-name $cdnProfileName --resource-group $resourceGroupName --query "[0].hostName" --output tsv
+        $endpointExists | Should -Be $endpointName
+    }
+}
+
 # Test to validate Static WebSite and its content.
 Describe 'static-website' {
     Write-Host "WebsiteHostName: $endpointName"  
